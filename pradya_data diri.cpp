c0ae3c9f30076873126a10eee331e7956b082018
pradyaheningpertiwi <112565459+pradyaheningpertiwi@users.noreply.github.com>
2022-09-08 10:17:44 +0700

    Add files via upload

---
+++ pradya_data diri.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string nama;
+	string akt;
+	string jrsn;
+	cout<<"masukan nama : ";
+	getline(cin,nama);
+	cout<<"angkatan : ";
+	cin>>akt;
+	cout<<"jurusan : ";
+	cin>>jrsn;
+	
+	return 0;
+	
+}
