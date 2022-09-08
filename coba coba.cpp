8fa09662876f86922a03c59ee80f9fc41b20818e
pradyaheningpertiwi <112565459+pradyaheningpertiwi@users.noreply.github.com>
2022-09-09 05:56:37 +0700

    Add files via upload

---
+++ coba coba.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int nomor = 2022;
+	string jurusan;
+	
+	cout<<"saya adalah angkatan : "<<nomor<<endl;
+	cout<<"jurusan saya adalah : "<<jurusan;
+	getline(cin,jurusan);
+	
+	
+	return 0;
+	
+}
